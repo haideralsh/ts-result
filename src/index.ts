d397d77a111021efd10793e4cd25332c6b500c37
Haïder Alshamma <haideralsh@gmail.com>
2021-02-18 20:30:58 -0500

    Add code documentation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
+/**
+ * A type that describes the properties and functions available directly without
+ * using the `ok` property as discriminator for results wrapped with either an
+ * {@link Ok} or an {@link Err}.
+ */
 type Uncertain = {
+    ok: boolean
     getOr: (defaultValue: any) => any
     getOrRun: <S>(fn: () => S) => any
     getOrThrow: (err?: Error | string) => any
     mapWithDefault: <S, R>(defaultValue: S, fn: (parameter: any) => R) => R
 }
 
+/**
+ * A type that describes the properties and functions available for results
+ * wrapped with an {@link Ok} where the `ok` discriminator is true
+ */
 type Ok<T> = {
     ok: true
     get: () => T
@@ -16,9 +26,13 @@ type Ok<T> = {
     mapWithDefault: <S, R>(defaultValue: S, fn: (parameter: T) => R) => R
 } & Uncertain
 
-type Err<T> = {
+/**
+ * A type that describes the properties and functions available for results
+ * wrapped with an {@link Err} where the `ok` discriminator is false
+ */
+type Err<E> = {
     ok: false
-    getError: () => T
+    getError: () => E
 
     getOr: <S>(defaultValue: S) => S
     getOrRun: <S>(fn: () => S) => S
@@ -26,8 +40,16 @@ type Err<T> = {
     mapWithDefault: <S, R>(defaultValue: S, fn: (parameter: S) => R) => R
 } & Uncertain
 
+/**
+ * A union type for objects of type {@link Ok} or {@link Err}. The discriminator
+ * for whether the object is `Ok` or `Err` is the `ok` property.
+ */
 export type Result<T, E> = Ok<T> | Err<E>
 
+/**
+ * The Ok function takes a value of type `T` and returns an object with the type
+ * of `Ok<T>`
+ */
 export const Ok = <T>(value: T): Ok<T> => ({
     ok: true,
     get: () => value,
@@ -40,7 +62,11 @@ export const Ok = <T>(value: T): Ok<T> => ({
         fn(value),
 })
 
-export const Err = <T>(errorValue: T): Err<T> => ({
+/**
+ * The Err function takes a value of type `E` and returns an object with the
+ * type of `Err<E>`
+ */
+export const Err = <E>(errorValue: E): Err<E> => ({
     ok: false,
     getError: () => errorValue,
 
